d35569e1392741d3f6f44fbfb1522166d2b4b043
sanuv9683 <sanuv9683@gmail.com>
2022-11-09 11:55:00 +0530

    Content Updated

--- JavaEE-59/01_Hello-JavaEE/src/MyClass.java
+++ JavaEE-59/01_Hello-JavaEE/src/MyClass.java
@@ -4,12 +4,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/customer")
 public class MyClass extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Hello there, Method Invoked");
+        String id=req.getParameter("id");
+        String name=req.getParameter("name");
+        String address=req.getParameter("address");
+        String salary=req.getParameter("salary");
+        System.out.println(id+" "+name+ " "+address+" "+salary);
+
+        PrintWriter writer = resp.getWriter();
+        writer.write("<h1>Hello I am your response</h1>");
+        writer.write("<ul>");
+        writer.write("<li>"+id+"</li>");
+        writer.write("<li>"+name+"</li>");
+        writer.write("<li>"+address+"</li>");
+        writer.write("<li>"+salary+"</li>");
+        writer.write("</ul>");
+
     }
 }
