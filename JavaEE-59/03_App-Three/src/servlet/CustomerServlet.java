e390a33296c446a0d3bf8e2f15f29f3ac25b90a9
sanuv9683 <sanuv9683@gmail.com>
2022-11-14 14:41:38 +0530

    Content Updated with crud

--- JavaEE-59/03_App-Three/src/servlet/CustomerServlet.java
+++ JavaEE-59/03_App-Three/src/servlet/CustomerServlet.java
@@ -1,4 +1,47 @@
 package servlet;
 
-public class CustomerServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@WebServlet(urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+        String salary = req.getParameter("salary");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "sanu1234");
+            PreparedStatement psmt = connection.prepareStatement("insert into Customer values(?,?,?,?)");
+            psmt.setObject(1,id);
+            psmt.setObject(2,name);
+            psmt.setObject(3,address);
+            psmt.setObject(4,salary);
+            boolean execute = psmt.executeUpdate()>0;
+            resp.getWriter().write("<h1>"+execute+"</h1>");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
