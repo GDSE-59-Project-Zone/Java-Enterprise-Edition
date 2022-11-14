cc411570f6f3687261ecc4598d67dfd1d57d68e1
sanuv9683 <sanuv9683@gmail.com>
2022-11-14 09:44:43 +0530

    Content Updated

---
+++ JavaEE-59/02_App-Two/src/MyClass.java
@@ -0,0 +1,36 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/item")
+public class MyClass extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Request received");
+
+        String code = req.getParameter("code");
+        String name = req.getParameter("name");
+        String qty = req.getParameter("qty");
+        String price = req.getParameter("price");
+
+        System.out.println(code);
+        System.out.println(name);
+        System.out.println(qty);
+        System.out.println(price);
+
+        PrintWriter writer = resp.getWriter();
+        writer.write("<h1>Response Generated</h1>");
+        writer.write("<table border='2' width='100%'>");
+        writer.write("<thead>");
+        writer.write("<tr><th>Code</th><th>Name</th><th>Qty</th><th>Price</th></tr>");
+        writer.write("</thead>");
+        writer.write("<tbody>");
+        writer.write("<tr><td>"+code+"</td><td>"+name+"</td><td>"+qty+"</td><td>"+price+"</td></tr>");
+        writer.write("</tbody>");
+        writer.write("</table>");
+    }
+}
