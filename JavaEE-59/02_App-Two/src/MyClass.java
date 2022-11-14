d3e067c1035f646ed6c237277759c5ce29c9197e
sanuv9683 <sanuv9683@gmail.com>
2022-11-14 10:45:52 +0530

    Content Updated

--- JavaEE-59/02_App-Two/src/MyClass.java
+++ JavaEE-59/02_App-Two/src/MyClass.java
@@ -33,4 +33,30 @@ public class MyClass extends HttpServlet {
         writer.write("</tbody>");
         writer.write("</table>");
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Do Post Method Invoked");
+
+        String code = req.getParameter("code");
+        String name = req.getParameter("name");
+        String qty = req.getParameter("qty");
+        String price = req.getParameter("price");
+
+        System.out.println(code);
+        System.out.println(name);
+        System.out.println(qty);
+        System.out.println(price);
+
+        PrintWriter writer = resp.getWriter();
+        writer.write("<h1>Response Generated</h1>");
+        writer.write("<table border='2' width='100%'>");
+        writer.write("<thead>");
+        writer.write("<tr><th>Code</th><th>Name</th><th>Qty</th><th>Price</th></tr>");
+        writer.write("</thead>");
+        writer.write("<tbody>");
+        writer.write("<tr><td>"+code+"</td><td>"+name+"</td><td>"+qty+"</td><td>"+price+"</td></tr>");
+        writer.write("</tbody>");
+        writer.write("</table>");
+    }
 }
